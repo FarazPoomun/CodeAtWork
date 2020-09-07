4ad29a5b7b79eb5d7449921ae4099e226fb87c9f
Poomun, Faraz <faraz.poomun@ceridian.com>
2020-09-08 01:52:39 +0400

    New Navbar for APP

---
+++ CodeAtWork/Controllers/CodeAtWorkAppController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeAtWork.Controllers
+{
+    public class CodeAtWorkAppController : Controller
+    {
+        // GET: CodeAtWorkApp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
